c318be7ee64b293ee8d2a88109780c5f6dadc1c4
Mathijs Henquet <mathijs.henquet@gmail.com>
2023-04-10 09:23:33 +0200

    update

--- docs/time/script.js
+++ docs/time/script.js
@@ -1,15 +1,18 @@
 
-const $weeks = document.getElementById("weeks");
-const $days = document.getElementById("days");
 
 document.addEventListener("DOMContentLoaded", function() {
-    const startDate = moment("2022-11-14");
-    const currentDate = moment();
-    const duration = moment.duration(currentDate.diff(startDate));
-    
-    const weeks = Math.floor(duration.asWeeks());
-    const days = Math.floor(duration.asDays() % 7);
+    document.querySelectorAll("*[data-date-since]").forEach((elem) => {
+        const $weeks = elem.querySelector(".weeks");
+        const $days = elem.querySelector(".days");
 
-    $weeks.textContent = `${weeks} week${weeks === 1 ? '' : 's'}`;
-    $days.textContent = `${days} day${days === 1 ? '' : 's'}`;
+        const startDate = moment(elem.dataset.dateSince);
+        const currentDate = moment();
+        const duration = moment.duration(currentDate.diff(startDate));
+        
+        const weeks = Math.floor(duration.asWeeks());
+        const days = Math.floor(duration.asDays() % 7);
+    
+        $weeks.textContent = `${weeks} week${weeks === 1 ? '' : 's'}`;
+        $days.textContent = `${days} day${days === 1 ? '' : 's'}`;
+    })
 });
