4947bb9fd5bdb18350ee6924b4284452d6a2895f
Mathijs Henquet <mathijs.henquet@gmail.com>
2023-07-01 08:51:12 +0200

    update baby time

--- docs/time/script.js
+++ docs/time/script.js
@@ -1,6 +1,4 @@
-
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("*[data-date-since]").forEach((elem) => {
         const $weeks = elem.querySelector(".weeks");
         const $days = elem.querySelector(".days");
@@ -8,11 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
         const startDate = moment(elem.dataset.dateSince);
         const currentDate = moment();
         const duration = moment.duration(currentDate.diff(startDate));
-        
-        const weeks = Math.floor(duration.asWeeks());
-        const days = Math.floor(duration.asDays() % 7);
-    
-        $weeks.textContent = `${weeks} week${weeks === 1 ? '' : 's'}`;
-        $days.textContent = `${days} day${days === 1 ? '' : 's'}`;
-    })
+
+        if ($days) {
+            const weeks = Math.floor(duration.asWeeks());
+            const days = Math.floor(duration.asDays() % 7);
+
+            $weeks.textContent = `${weeks} week${weeks === 1 ? "" : "s"}`;
+            $days.textContent = `${days} day${days === 1 ? "" : "s"}`;
+        } else {
+            const weeks = Math.round(duration.asWeeks());
+            $weeks.textContent = `${weeks} week${weeks === 1 ? "" : "s"}`;
+        }
+    });
 });
